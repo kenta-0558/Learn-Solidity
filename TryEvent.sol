3a28c31a48b78e176611dfb74cacdb1c41f4421e
kenta0558 <mannheim42@yahoo.de>
2021-04-04 22:29:11 +0200

    add new contract to try event into TryEvent.sol

--- TryEvent.sol
+++ TryEvent.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.9.0;
 
-// https://ethereum.org/ig/developers/tutorials/logging-events-smart-contracts/
+//https://ethereum.org/ig/developers/tutorials/logging-events-smart-contracts/
 
 contract Counter {
     
@@ -18,3 +18,30 @@ contract Counter {
         return count;
     }
 }
+
+//https://www.bitdegree.org/learn/solidity-events
+
+contract SimpleAuction {
+    event HighestBidIncreased(address bidder, uint amount);
+    
+    function bid() public payable {
+        
+        emit HighestBidIncreased(msg.sender, msg.value);
+    }
+}
+
+// contract clientReceipt {
+//     event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
+    
+//     function deposit(bytes32 _id) public payable {
+        
+//         emit Deposit(msg.sender, _id, msg.value);
+//     }
+// }
+
+// https://github.com/ethchange/smart-exchange/blob/master/lib/contracts/SmartExchange.sol
+
+// contract TestEvent {
+    
+//     event Transfer()
+// }
