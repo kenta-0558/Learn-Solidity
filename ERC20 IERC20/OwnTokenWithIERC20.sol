217b9f90716aea5112bf4dde7d499b4fdf8ac658
kenta0558 <mannheim42@yahoo.de>
2021-04-07 20:38:47 +0200

    add balances state and balanceOf function in contract and interface in OwnTokenWithERC20.sol

--- ERC20 IERC20/OwnTokenWithIERC20.sol
+++ ERC20 IERC20/OwnTokenWithIERC20.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.16 <0.9.0;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
 }
 
 contract KiichiToken is IERC20 {
@@ -13,13 +14,20 @@ contract KiichiToken is IERC20 {
     string public constant symbol = "KTT";
     uint8 public constant decimals = 18;
     
+    mapping (address => uint256) balances;
+    
     uint256 totalSupply_;
     
     constructor(uint256 _totalSupply) {
         totalSupply_ = _totalSupply;
+        balances[msg.sender] = totalSupply_;
     }
     
     function totalSupply() public override view returns (uint256) {
         return totalSupply_;
+    }
+    
+    function balanceOf(address _accountOwner) public override view returns (uint256) {
+        return balances[_accountOwner];
     }
 }
