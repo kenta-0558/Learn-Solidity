bd8f8391f9a695c590db5d9fac27491a27b6bacd
kenta0558 <mannheim42@yahoo.de>
2021-04-07 16:49:49 +0200

    add source url

--- ERC20 IERC20/OwnTokenWithERC20.sol
+++ ERC20 IERC20/OwnTokenWithERC20.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
+// learn from 
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol 
+// https://forum.openzeppelin.com/t/deploy-a-simple-erc20-token-in-remix/1203
+// https://forum.openzeppelin.com/t/simple-erc20-token-example/4403
+
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract KiichiToken is ERC20 {
