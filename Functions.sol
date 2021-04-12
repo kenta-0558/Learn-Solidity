ac554fda1caf23b729a177a65b24793bd528a571
kenta0558 <mannheim42@yahoo.de>
2021-04-12 23:31:10 +0200

    add new File Functions.sol to try some Functions of solidity

---
+++ Functions.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract FunctionTest {
+    
+    function calculationA(uint _a, uint _b) public pure returns (uint _sumA, uint _sumB) {
+        
+        require(_a > _b);
+        
+        _sumA = _a + _b;
+        _sumB = _a  - _b;
+    }   
+}
