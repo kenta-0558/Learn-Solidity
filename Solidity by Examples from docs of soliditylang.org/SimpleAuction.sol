ee2e02102bfed0d8a312c769c13751b9fc78faaa
kenta0558 <mannheim42@yahoo.de>
2021-04-16 23:17:16 +0200

    add new file SimpleAuction and create state variables in SimpleAuction contract

---
+++ Solidity by Examples from docs of soliditylang.org/SimpleAuction.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SimpleAuction {
+    
+    address payable public beneficiary;
+    uint public auctionEndTime;
+    
+    address public highestBidder;
+    uint public highestBid;
+}
