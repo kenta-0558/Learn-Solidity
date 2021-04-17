f57405d5b5e66f32d1f76d74a6a38175071ce7a3
kenta0558 <mannheim42@yahoo.de>
2021-04-17 21:51:30 +0200

    add new function withdraw into SimpleAuction contract

--- Solidity by Examples from docs of soliditylang.org/SimpleAuction.sol
+++ Solidity by Examples from docs of soliditylang.org/SimpleAuction.sol
@@ -38,4 +38,21 @@ contract SimpleAuction {
         
         emit HighestBidIncreased(msg.sender, msg.value);
     }
+
+    function withdraw() public returns (bool) {
+        
+        uint amount = pendingReturns[msg.sender];
+        
+        if (amount > 0) {
+            
+            pendingReturns[msg.sender] = 0;
+            
+            if (!payable(msg.sender).send(amount)) {
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
