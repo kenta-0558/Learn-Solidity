ab79be9b7dd4ec3f825bb82436ab83efd507d3b3
kenta0558 <mannheim42@yahoo.de>
2021-04-17 13:34:29 +0200

    create new file CrowdFunding and add new contract CrowdFunding with 2 struct type into the file

---
+++ Solidity by Examples from docs of soliditylang.org/CrowdFunding.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract CrowdFunding {
+    
+    struct Funder {
+        address funderAddress;
+        uint amount;
+    }
+    
+    struct Campaign {
+        address payable beneficiary;
+        uint fundingGoal;
+        uint numfunders;
+        uint amount;
+        mapping (uint => Funder) funders;
+    }
+}
