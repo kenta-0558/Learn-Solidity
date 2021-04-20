74e583ff2b290655f670b5f78b1cb8aca31cb755
kenta0558 <mannheim42@yahoo.de>
2021-04-20 22:19:11 +0200

    delete owner from Ticket struct and add _createTicket function into TicketBase contract to issue new Ticket

--- Write own contract from real apps/Tickets.sol
+++ Write own contract from real apps/Tickets.sol
@@ -5,16 +5,28 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TicketBase {
     
     struct Ticket {
-        address owner;
         uint area;
-        string durations;
+        string ticketType;
         uint createdAt;
     }
     
     Ticket[] public tickets;
     
     mapping(uint => address) public ticketIndexToOwner;
-
+    
     event IssueNewTicket(address owner, uint ticketID, uint createdAt);
     event CancelTicket(address owner, uint ticketID, uint canceledAt);
+    
+    function _createTicket(uint _area, string memory _ticketType) internal returns (uint _ticketID) {
+        
+        tickets.push(Ticket({
+            area: _area,
+            ticketType: _ticketType,
+            createdAt: block.timestamp
+        }));
+        
+        _ticketID = tickets.length - 1;
+        
+        ticketIndexToOwner[_ticketID] = msg.sender;
+    }
 }
