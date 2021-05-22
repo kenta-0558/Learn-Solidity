8fc33cd963640ffb558ca82e71c2fb4a44a6b51d
kenta0558 <mannheim42@yahoo.de>
2021-05-22 13:35:14 +0200

    add frontend code to call initialize function of contract as comment

--- AbiDecode.sol
+++ AbiDecode.sol
@@ -25,3 +25,35 @@ contract AbiDecode {
         return true;
     }
 }
+
+// parameter to call initialize function of contract from frontend with web3
+// const Web3 = require('web3');
+
+// let web3 = new Web3("http://localhost:8545");
+
+// const getEncoded = (name, age, nationality) => {
+
+//     const parameter = {
+//         name,
+//         age,
+//         nationality
+//     };
+
+//     const encoded = web3.eth.abi.encodeParameter(
+//         {
+//             "Person": {
+//                 "name": "string",
+//                 "age": "uint",
+//                 "nationality": "string"
+//             },
+//         },
+//         parameter
+//     ); 
+
+//     return encoded;
+// }
+
+// const encoded = getEncoded("Ichina", 4, "japanese");
+
+// console.log(encoded);
+
