2103ad5a29c3c605925e40671c0e8ad2f24dcf9d
kenta0558 <mannheim42@yahoo.de>
2021-06-04 23:17:07 +0200

    add sendToPool and returnFromPool into IKTTToken

--- ERC20 IERC20/KTTToken.sol
+++ ERC20 IERC20/KTTToken.sol
@@ -6,6 +6,9 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 interface IKTTToken is IERC20 {
     event KTTTokenBalanceUpdated(address _user, uint _amount);
+
+    function sendToPool(address _sender,  address poolAddress, uint256 _amount) external;
+    function returnFromPool(address poolAddress, address user, uint256 _amount ) external;
     
     // function mint(address _account, uint256 _amount) external;
     // function burn(address _account, uint256 _amount) external;    
