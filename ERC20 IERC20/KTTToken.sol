74dfd5a47b400cd32f89e0d1a63ad319c7c00dc5
kenta0558 <mannheim42@yahoo.de>
2021-06-04 23:43:13 +0200

    add mint and burn into IKTTToken and remove returnFromPool from IKTTTOken

--- ERC20 IERC20/KTTToken.sol
+++ ERC20 IERC20/KTTToken.sol
@@ -8,10 +8,10 @@ interface IKTTToken is IERC20 {
     event KTTTokenBalanceUpdated(address _user, uint _amount);
 
     function sendToPool(address _sender,  address poolAddress, uint256 _amount) external;
-    function returnFromPool(address poolAddress, address user, uint256 _amount ) external;
+    // function returnFromPool(address poolAddress, address user, uint256 _amount ) external;
     
-    // function mint(address _account, uint256 _amount) external;
-    // function burn(address _account, uint256 _amount) external;    
+    function mint(address _account, uint256 _amount) external;
+    function burn(address _account, uint256 _amount) external;    
 } 
 
 contract Pool {
