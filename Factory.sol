0d8380db63688d62eb305f087ad10ce12c1b2cf6
kenta0558 <mannheim42@yahoo.de>
2021-06-16 20:42:23 +0200

    add new contract PlanFactory with struct type and state variable

--- Factory.sol
+++ Factory.sol
@@ -6,3 +6,12 @@ interface TemplateInterface {
     event Initialized(bytes indexed abiBytes);
     function intialize(bytes memory abiBytes) external returns (bool);    
 }
+
+contract PlanFactory {
+    
+    mapping(string => address) public templates;
+    
+    uint nonce = 0;
+    
+    
+}
